b4c048c0e092576448f30c4d1a6eeeb6b99f5e70
Jaden Arceneaux <arceneauxjaden@gmail.com>
2020-06-01 19:11:07 -0700

    fixed docs typo

--- MedHelp.js
+++ MedHelp.js
@@ -1,20 +1,23 @@
 const fs = require('fs')
 
-class MedHelp{
+class MedHelp {
   constructor(path = './data') {
     this.path = path;
     this.patient = require(path);
 	}
 
   append(name, time, amount) {
+    // adds/updates entry to json
 		this.patient[name] = {
       "time": time,
       "amount": amount
     };
 	}
 
-  write(path = this.path){
-    const fs = require('fs');
+  write(path = this.path) {
+    // formatting and writing json to path
+    const fs = require('fs'); // imports filesystem
+    // writes file to path using indent size of 2
     fs.writeFile(path, JSON.stringify(this.patient, null, 2), 
       err => { 
       // Checking for errors 
